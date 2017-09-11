51da1d52643fbc4b1fb407291c2478267d46e41d
Zhihua Ban <sawpara@126.com>
2017-09-11 17:08:38 +0800

    change to my code of sm numbers

--- src/main.cpp
+++ src/main.cpp
@@ -12,44 +12,24 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
-// Beginning of GPU Architecture definitions
-inline int _ConvertSMVer2Cores(int major, int minor)
-{
-	// Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
-	typedef struct
-	{
-		int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
-		int Cores;
-	} sSMtoCores;
-
-	sSMtoCores nGpuArchCoresPerSM[] =
-	{
-		{ 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
-		{ 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
-		{ 0x30, 192 }, // Kepler Generation (SM 3.0) GK10x class
-		{ 0x32, 192 }, // Kepler Generation (SM 3.2) GK10x class
-		{ 0x35, 192 }, // Kepler Generation (SM 3.5) GK11x class
-		{ 0x37, 192 }, // Kepler Generation (SM 3.7) GK21x class
-		{ 0x50, 128 }, // Maxwell Generation (SM 5.0) GM10x class
-		{ 0x52, 128 }, // Maxwell Generation (SM 5.2) GM20x class
-		{ -1, -1 }
-	};
-
-	int index = 0;
-
-	while (nGpuArchCoresPerSM[index].SM != -1)
-	{
-		if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor))
-		{
-			return nGpuArchCoresPerSM[index].Cores;
-		}
-		index++;
-	}
-
-	// If we don't find the values, we default use the previous one to run properly
-	printf("MapSMtoCores for SM %d.%d is undefined.  Default to use %d Cores/SM\n", major, minor, nGpuArchCoresPerSM[index - 1].Cores);
-	return nGpuArchCoresPerSM[index - 1].Cores;
+inline int get_num_sm_cores(int major, int minor){
+  switch ((major<<4)+minor){
+    case 0x20: return 32;  // Fermi   (SM 2.0) GF100 class
+    case 0x21: return 48;  // Fermi   (SM 2.1) GF10x class
+    case 0x30: return 192; // Kepler  (SM 3.0) GK10x class
+    case 0x32: return 192; // Kepler  (SM 3.2) GK10x class
+    case 0x35: return 192; // Kepler  (SM 3.5) GK11x class
+    case 0x37: return 192; // Kepler  (SM 3.7) GK21x class
+    case 0x50: return 128; // Maxwell (SM 5.0) GM10x class
+    case 0x52: return 128; // Maxwell (SM 5.2) GM20x class
+    case 0x53: return 128; // Maxwell (SM 5.3) GM20x class
+    case 0x60: return 64;  // Pascal  (SM 6.0) GP100 class
+    case 0x61: return 128; // Pascal  (SM 6.1) GP10x class
+    case 0x62: return 128; // Pascal  (SM 6.2) GP10x class
+    default: return -1;
+  }
 }
+
 #include "tools.hpp"
 
 class Evaluate
@@ -107,7 +87,7 @@ public:
 			
 			cout << "##################################################################" << endl;
 			cout << "GPU "<< g << " Configuration: " << endl;
-			cout << "number of cores : " << cudaprop.multiProcessorCount << "x" << _ConvertSMVer2Cores(cudaprop.major, cudaprop.minor) << "=" << cudaprop.multiProcessorCount * _ConvertSMVer2Cores(cudaprop.major, cudaprop.minor) << endl;
+			cout << "number of cores : " << cudaprop.multiProcessorCount << "x" << get_num_sm_cores(cudaprop.major, cudaprop.minor) << "=" << cudaprop.multiProcessorCount * get_num_sm_cores(cudaprop.major, cudaprop.minor) << endl;
 			cout << "GPU name        : " << cudaprop.name << endl;
 			cout << "Clock Rate      : " << double(cudaprop.clockRate) / 1024. << "MHz" << endl;
 			cout << "Global Memory   : " << double(cudaprop.totalGlobalMem) / 1024. / 1024. / 1024. << "GB" << endl;
